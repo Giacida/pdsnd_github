08a26043d07a73d5288e869cefa6dbefe3f9a699
Giada <giadawing@gmail.com>
2020-12-13 18:04:42 +0100

    Fix stats of birth year

--- bikeshare.py
+++ bikeshare.py
@@ -247,8 +247,8 @@ def user_stats(city, df):
         print('\nDATA ABOUT GENDER ARE NOT AVAILABLE')
 
     if 'Birth Year' in df.columns:
-        print('\nBIRTH YEAR OF YOUNGEST USER: ', int(df['Birth Year'].min()))
-        print('BIRTH YEAR OF OLDEST USER: ', int(df['Birth Year'].max()))
+        print('\nBIRTH YEAR OF OLDEST USER: ', int(df['Birth Year'].min()))
+        print('BIRTH YEAR OF YOUNGEST USER: ', int(df['Birth Year'].max()))
         print('MOST COMMON BIRTH YEAR: ',int((df['Birth Year'].mean())))
     else:
         print('\nDATA ABOUT BIRTH YEAR ARE NOT AVAILABLE')
